44bf602dc352d27e306f0df7931741b6ca87cb05
jesscharalel <109556742+jesscharalel@users.noreply.github.com>
2022-07-22 13:33:14 -0400

    yaaas

---
+++ day3.py
@@ -0,0 +1,17 @@
+class year:
+    def __init__(self, year,age):
+        self.year=year
+        self.age=age
+    def myfunc (self):
+        print("This student's year is" + self.year, "My age is ", self.age)
+p1= year("7th", 14)
+p1.myfunc()
+
+class course:
+    def__init__(self, course,age)
+    self.course=course
+    self.age=age
+    def myfunc(self):
+        print("This student's course is" + self.course,"My age is ", self.age)
+p1=course("Physics", 16)
+p1.myfunc()
